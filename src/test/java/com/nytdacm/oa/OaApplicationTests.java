cf4715cda701c2149eac7eba9d2bd7a74931f9fc
Taoning Ge <gtn1024o@outlook.com>
2023-01-24 18:44:42 +0800

    创建 Spring Boot 项目

---
+++ src/test/java/com/nytdacm/oa/OaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nytdacm.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
