9c37be526e650c17d7ca30c01765a3c135e8341b
Taoning Ge <gtn1024o@outlook.com>
2023-01-24 22:37:39 +0800

    引入Checkstyle代码质量检查

--- src/main/java/com/nytdacm/oa/controller/AuthController.java
+++ src/main/java/com/nytdacm/oa/controller/AuthController.java
@@ -10,7 +10,12 @@ import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
--- src/main/java/com/nytdacm/oa/model/entity/User.java
+++ src/main/java/com/nytdacm/oa/model/entity/User.java
@@ -1,8 +1,15 @@
 package com.nytdacm.oa.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.*;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
 
 import java.util.Objects;
 
