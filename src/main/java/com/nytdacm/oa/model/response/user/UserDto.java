c8aef770f1e78285b899a940b887a6d8794b4ba5
Taoning Ge <gtn1024o@outlook.com>
2023-01-27 13:53:49 +0800

    完善User结构体

--- src/main/java/com/nytdacm/oa/model/response/user/UserDto.java
+++ src/main/java/com/nytdacm/oa/model/response/user/UserDto.java
@@ -3,13 +3,17 @@ package com.nytdacm.oa.model.response.user;
 public record UserDto(
     Long userId,
     String username,
-    String name
+    String name,
+    boolean superAdmin,
+    boolean admin
 ) {
     public static UserDto fromEntity(com.nytdacm.oa.model.entity.User user) {
         return new UserDto(
             user.getUserId(),
             user.getUsername(),
-            user.getName()
+            user.getName(),
+            user.isSuperAdmin(),
+            user.isAdmin()
         );
     }
 }
