3d3b90e0d8ae820032b640cc6ad447b7539897ea
Taoning Ge <gtn1024o@outlook.com>
2023-01-30 22:37:52 +0800

    修改激活提示语

--- src/main/java/com/nytdacm/oa/service/impl/AuthServiceImpl.java
+++ src/main/java/com/nytdacm/oa/service/impl/AuthServiceImpl.java
@@ -27,7 +27,7 @@ public class AuthServiceImpl implements AuthService {
     public String login(String username, String password) {
         var user = userService.getUserByUsername(username);
         if (!user.isActive()) {
-            throw new OaBaseException("用户未激活", 401);
+            throw new OaBaseException("用户未激活，请耐心等待管理员激活", 401);
         }
         if (!PasswordUtil.checkPassword(password, user.getPasswordSalt(), user.getPassword())) {
             throw new OaBaseException("密码错误", 401);
