0ae103a11b3525ad231782e0ac5a6a664d60457c
Taoning Ge <gtn1024o@outlook.com>
2023-02-01 23:59:11 +0800

    make checkstyle happy

--- src/main/java/com/nytdacm/oa/controller/admin/AdminUserController.java
+++ src/main/java/com/nytdacm/oa/controller/admin/AdminUserController.java
@@ -14,7 +14,14 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.util.Objects;
