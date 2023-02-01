5a27cea29fd0b63965fb5dc023a1421d84b5bb39
Taoning Ge <gtn1024o@outlook.com>
2023-02-01 16:56:05 +0800

    添加SaToken拦截器

--- src/main/java/com/nytdacm/oa/config/WebMvcConfig.java
+++ src/main/java/com/nytdacm/oa/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.nytdacm.oa.config;
 
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api", c -> c.isAnnotationPresent(RestController.class));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册 Sa-Token 拦截器
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/api/**");
     }
 }
