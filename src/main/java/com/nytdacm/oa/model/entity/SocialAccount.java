64549d22f7afd17170c430e5fdf67f541a986caf
Taoning Ge <gtn1024o@outlook.com>
2023-02-07 22:40:12 +0800

    使用Lombok

--- src/main/java/com/nytdacm/oa/model/entity/SocialAccount.java
+++ src/main/java/com/nytdacm/oa/model/entity/SocialAccount.java
@@ -1,7 +1,12 @@
 package com.nytdacm.oa.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
 public class SocialAccount implements Serializable {
     private String codeforces;
     private Boolean codeforcesCrawlerEnabled;
@@ -9,92 +14,12 @@ public class SocialAccount implements Serializable {
     private Integer codeforcesMaxRating;
     private String codeforcesRank;
 
-    public String getCodeforces() {
-        return codeforces;
-    }
-
-    public void setCodeforces(String codeforces) {
-        this.codeforces = codeforces;
-    }
-
-    public Boolean getCodeforcesCrawlerEnabled() {
-        return codeforcesCrawlerEnabled;
-    }
-
-    public void setCodeforcesCrawlerEnabled(Boolean codeforcesCrawlerEnabled) {
-        this.codeforcesCrawlerEnabled = codeforcesCrawlerEnabled;
-    }
-
-    public Integer getCodeforcesRating() {
-        return codeforcesRating;
-    }
-
-    public void setCodeforcesRating(Integer codeforcesRating) {
-        this.codeforcesRating = codeforcesRating;
-    }
-
-    public Integer getCodeforcesMaxRating() {
-        return codeforcesMaxRating;
-    }
-
-    public void setCodeforcesMaxRating(Integer codeforcesMaxRating) {
-        this.codeforcesMaxRating = codeforcesMaxRating;
-    }
-
-    public String getCodeforcesRank() {
-        return codeforcesRank;
-    }
-
-    public void setCodeforcesRank(String codeforcesRank) {
-        this.codeforcesRank = codeforcesRank;
-    }
-
     private String github;
-
-    public String getGithub() {
-        return github;
-    }
-
-    public void setGithub(String github) {
-        this.github = github;
-    }
 
     private String website;
 
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
     private String atCoder;
-
-    public String getAtCoder() {
-        return atCoder;
-    }
-
-    public void setAtCoder(String atCoder) {
-        this.atCoder = atCoder;
-    }
 
     private String luogu;
     private Boolean luoguCrawlerEnabled;
-
-    public String getLuogu() {
-        return luogu;
-    }
-
-    public void setLuogu(String luogu) {
-        this.luogu = luogu;
-    }
-
-    public Boolean getLuoguCrawlerEnabled() {
-        return luoguCrawlerEnabled;
-    }
-
-    public void setLuoguCrawlerEnabled(Boolean luoguCrawlerEnabled) {
-        this.luoguCrawlerEnabled = luoguCrawlerEnabled;
-    }
 }
