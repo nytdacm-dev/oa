ad3793ad630bfaacc30b2cf314086970dac81db9
Taoning Ge <gtn1024o@outlook.com>
2023-02-08 22:01:12 +0800

    编写数据库备份脚本

---
+++ bin/backup_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+db_container_name=db-for-oa
+user=$(whoami)
+filename=nytdacm_oa_dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+backup_dir=/home/$user/backups/
+
+mkdir -p $backup_dir
+
+docker exec -t $db_container_name pg_dumpall -c -U nytdacm_oa | gzip > $backup_dir/nytdacm_oa_dump_`date +%Y-%m-%d"_"%H_%M_%S`.gz
