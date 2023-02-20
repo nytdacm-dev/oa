d624505ad38e5e0274f0a2618fbbf2c5022c67a2
Taoning Ge <gtn1024o@outlook.com>
2023-02-20 13:24:34 +0800

    修改

--- oa-third-part/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation(project(":oa-common"))
-    implementation(project(":oa-dao"))
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
-    implementation("org.jsoup:jsoup:1.15.4")
-}
