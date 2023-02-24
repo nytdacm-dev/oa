3a02e1e7aa4c4ee5b822583a2bccfc3c695765da
Taoning Ge <gtn1024o@outlook.com>
2023-02-24 20:24:13 +0800

    使用Kotlin改写前台队员展示Controller

---
+++ oa-app/src/main/kotlin/com/nytdacm/oa/controller/GroupController.kt
@@ -0,0 +1,23 @@
+package com.nytdacm.oa.controller
+
+import com.nytdacm.oa.response.HttpResponse
+import com.nytdacm.oa.response.group.GroupDto
+import com.nytdacm.oa.service.GroupService
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/group")
+open class GroupController(
+    private val groupService: GroupService,
+) {
+    @GetMapping("/homepage")
+    @Cacheable(value = ["homepageGroups"], key = "#root.methodName")
+    open fun homepageGroups(): ResponseEntity<HttpResponse<List<GroupDto>>> {
+        val groups = groupService.getAllGroups(true).map { GroupDto.fromEntity(it) }
+        return HttpResponse.success(200, "获取成功", groups)
+    }
+}
