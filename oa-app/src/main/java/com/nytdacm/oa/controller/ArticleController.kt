9edfde96f7494350eecbc106a3edc1982baf4925
Taoning Ge <gtn1024o@outlook.com>
2023-03-19 03:04:53 +0000

    开发文章编写接口

--- oa-app/src/main/java/com/nytdacm/oa/controller/ArticleController.kt
+++ oa-app/src/main/java/com/nytdacm/oa/controller/ArticleController.kt
@@ -1,13 +1,19 @@
 package com.nytdacm.oa.controller
 
+import cn.dev33.satoken.annotation.SaCheckLogin
+import cn.dev33.satoken.stp.StpUtil
 import com.nytdacm.oa.entity.Article
+import com.nytdacm.oa.entity.User
 import com.nytdacm.oa.response.HttpResponse
 import com.nytdacm.oa.response.ListWrapper
 import com.nytdacm.oa.response.user.UserDto
 import com.nytdacm.oa.service.ArticleService
+import com.nytdacm.oa.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/article")
 class ArticleController(
     private val articleService: ArticleService,
+    private val userService: UserService,
 ) {
     @GetMapping
     fun articles(
@@ -36,6 +43,19 @@ class ArticleController(
     ): ResponseEntity<HttpResponse<ArticleDto>> {
         return HttpResponse.success(200, "获取成功", articleService.getArticle(id).let { ArticleDto.toDto(it) })
     }
+
+    @PostMapping
+    @SaCheckLogin
+    fun writeArticle(
+        @RequestBody(required = true) newArticleRequest: NewArticleRequest
+    ): ResponseEntity<HttpResponse<ArticleDto>> {
+        val id = StpUtil.getLoginIdAsLong()
+        val user: User = userService.getUserById(id)
+        val article = Article(newArticleRequest.title, newArticleRequest.content, user)
+        return HttpResponse.success(200, "发表成功",
+            articleService.newArticle(article).let { ArticleDto.toDto(it) }
+        )
+    }
 }
 
 data class ArticleDto(
@@ -49,3 +69,8 @@ data class ArticleDto(
             ArticleDto(article.articleId, article.title, article.content, UserDto.fromEntity(article.author))
     }
 }
+
+data class NewArticleRequest(
+    val title: String,
+    val content: String,
+)
