012e603e13557c818d81b75b75fc1b70082915d8
Taoning Ge <gtn1024o@outlook.com>
2023-07-02 12:11:09 +0800

    修复没有无参构造引发的JPA问题的bug

--- oa-common/src/main/java/com/nytdacm/oa/entity/Article.java
+++ oa-common/src/main/java/com/nytdacm/oa/entity/Article.java
@@ -9,6 +9,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Article extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
